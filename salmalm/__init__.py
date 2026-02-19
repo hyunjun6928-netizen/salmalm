0dd35d2379c25c3c75327705a4f17883538639cf
hyunjun6928-netizen <hyunjun6928@gmail.com>
2026-02-19 20:49:42 +0900

    v0.12.6: Google OAuth settings UI
    - Add Google Calendar & Gmail OAuth2 settings card
    - Client ID / Client Secret input with save
    - Connect / Disconnect buttons
    - Auto status check
    - Setup guide
    - Version bump to 0.12.6

--- salmalm/__init__.py
+++ salmalm/__init__.py
@@ -4,9 +4,9 @@ import sys
 
 try:
     from .constants import VERSION
-    __version__ = '0.12.5'
+    __version__ = '0.12.6'
 except Exception:
-    __version__ = '0.12.5'
+    __version__ = '0.12.6'
 
 log = logging.getLogger('salmalm')
 app = None  # Will be set below if runtime (not during build)
