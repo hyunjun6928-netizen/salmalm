440f26d549d109d9c11d390224dfa2170674d88a
hyunjun6928-netizen <hyunjun6928@gmail.com>
2026-02-23 22:53:40 +0900

    v0.18.89: fix selftest NameError (clean rebuild)

--- salmalm/__init__.py
+++ salmalm/__init__.py
@@ -2,9 +2,9 @@ import logging
 import sys
 
 try:
-    __version__ = "0.18.88"
+    __version__ = "0.18.89"
 except Exception as e:  # noqa: broad-except
-    __version__ = "0.18.88"
+    __version__ = "0.18.89"
 
 log = logging.getLogger("salmalm")
 log.addHandler(logging.NullHandler())  # Prevent "No handlers" warning at import
