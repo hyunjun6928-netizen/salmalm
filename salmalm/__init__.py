0f53dbd5f0074bab6a4e8546fca2b75af806a276
hyunjun6928-netizen <hyunjun6928@gmail.com>
2026-02-23 22:47:45 +0900

    v0.18.87: structural improvements + CI gates + docs

--- salmalm/__init__.py
+++ salmalm/__init__.py
@@ -2,9 +2,9 @@ import logging
 import sys
 
 try:
-    __version__ = "0.18.86"
+    __version__ = "0.18.87"
 except Exception as e:  # noqa: broad-except
-    __version__ = "0.18.86"
+    __version__ = "0.18.87"
 
 log = logging.getLogger("salmalm")
 log.addHandler(logging.NullHandler())  # Prevent "No handlers" warning at import
